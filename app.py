80c75cc447be53074144c5e08fa4a8659825656f
Smalik182 <hssnmlk321@hotmail.com>
2022-07-14 11:49:40 -0500

    finished

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 # To activate our enviroment: .\env\Scripts\activate in the terminal
+
+# for error "cannot be loaded because running scripts is disabled on this system"
+# run: Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted, in terminal
 
 
 
@@ -29,11 +32,58 @@ class Todo(db.Model):
 
 # Create an idex route so that when we browse to the url we don't immediately 404
 # you set up routes in Flask with the app.route decorator
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 
 # Define function for that route
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content =request.form['content']   # logic for adding a task, form we created in index.html
+        new_task = Todo(content = task_content) # todo object, that will have the contents = task_content
+
+        # Here we try to push to our database
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()    # looks at all database contents created and returnds all of themm
+        return render_template('index.html', tasks = tasks)
+
+
+# Delete route for our database
+@app.route('/delete/<int:id>')          # We delete through the int ID in Todo
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)      # attempt to get the task by that id, if it doesn't work, 404
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting that task'
+
+
+# Update route for our database
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect ('/')
+        except:
+            return 'There was issue updating the task'
+    else:
+        return render_template('update.html', task=task)
+
 # Template Inheiritence: You create one master html  file that contains the skeleton of what each page will look
 # like and you just inheirit that in each other page
 
